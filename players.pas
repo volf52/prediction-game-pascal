edcb4e082cb501341402e2d3da24c8bc1f0133e4
Muhammad Arslan <rslnkrmt2552@gmail.com>
2017-12-31 17:48:31 +0300

    Made major changes to Bot Logic
    Restructured the way the bot gets the symbols for prediction and stuff.

--- players.pas
+++ players.pas
@@ -2,7 +2,7 @@ unit players;
 
 {
 	@author 	: Muhammad Arslan <rslnkrmt2552@gmail.com>
-	@purpose 	: Board operations for a simple Prediction game.
+	@purpose 	: Player operations for a simple Prediction game.
 }
 
 interface
@@ -10,7 +10,7 @@ interface
 
 	type
 		pType = (Human, Computer);
-		botC = array[1..4] of Integer;
+		coordinate_array = array[1..4] of Integer; {array of coordinates/inputs by player}
 		Player = record
 			player_name : record
 				firstname, lastname : String;				
@@ -18,22 +18,21 @@ interface
 			score : Integer;
 			case player_type : pType of
 				Human : ();
-				Computer : (level, elements : Integer; pred_list : predictionArray)
+				Computer : (level, elements : Integer; pred_list : predictionArray) {variable attributes in case of computer player only}
 			end;
 		
 
 
-	procedure getDetails(num : Integer; var p : Player);
-	procedure evaluate_human_turn(n, prediction_n: Integer; game_board : matrix; var x, y :Integer);
-	procedure popVal(index : Integer; var p : Player);
-	procedure clean_memory(board : matrix; var p : Player);
-	procedure find_matching(board : matrix; p : Player ; var x1, y1, x2, y2 : Integer);
-	function RandomRange(Low, High: LongInt) : LongInt;
-	function evaluate_bot_turn(n : Integer; game_board : matrix;  available : statusTracker; var p : Player) : botC;
+	procedure getDetails(num : Integer; var p : Player); {Get information to create an instant of a Player}
+	procedure evaluate_human_turn(n, prediction_n: Integer; game_board : matrix; var x, y :Integer); {Get correct input from a human player}
+	procedure popVal(index : Integer; var p : Player); {Pop the value at the given index, for a static array which is the player's prediction list}
+	procedure clean_memory(board : matrix; var p : Player); {Clear any non-usable cells from the player's memory}
+	procedure find_matching(board : matrix; p : Player ; var x1, y1, x2, y2 : Integer); {Find if there are any cells with matching value in memory}
+	function RandomRange(Low, High: LongInt) : LongInt; {Find a random number between the given range}
+	function evaluate_bot_turn(prediction_status : Boolean; n : Integer; game_board : matrix;  available : statusTracker; var p : Player) : coordinate_array; {evaluate turn for a bot}
 
 implementation	
 	
-
 	procedure getDetails(num : Integer; var p : Player);
 	var
 		tmp : Integer;
@@ -43,8 +42,8 @@ implementation
 
 				p.player_name.firstname := 'BOT';
 				case num of
-					1 : p.player_name.lastname := '_ONE';
-					2 : p.player_name.lastname := '_TWO';
+					1 : p.player_name.lastname := 'ONE';
+					2 : p.player_name.lastname := 'TWO';
 				end;
 				repeat
 					Writeln;
@@ -63,7 +62,7 @@ implementation
 				for tmp := 1 to MAX_PAIRS do 
 					begin
 						p.pred_list[tmp,1] := -13;
-						p.pred_list[tmp,2] := -13;
+						p.pred_list[tmp,2] := -13; {A placeholder value}
 					end;
 					
 			end
@@ -89,10 +88,11 @@ implementation
 	        readln(x);
 	        write('y --> ');
 	        readln(y);
-	        correct_input := (x in [1..n]) AND (y in [1..n]);
+	        correct_input := (x in [1..n]) AND (y in [1..n]); {Ensure inputs are within the range}
+	        valid_input := False;
 	        if correct_input then
 	        	begin
-	        		valid_input := (game_board[x-1, y-1].status = hidden);
+	        		valid_input := (game_board[x-1, y-1].status = hidden); {Ensure that the cell is not already being used}
 	        		if (not valid_input) then writeln('You should visit an eye doctor ... cause that box is already taken ...');
 	        	end
 	        	
@@ -106,12 +106,11 @@ implementation
 		var
 			i : Integer;
 		begin
-			i := index;
-			repeat
-				p.pred_list[i,1] := p.pred_list[i+1, 1];
-				p.pred_list[i,2] := p.pred_list[i+1, 2];
-				i := i + 1;
-			until ( (p.pred_list[i,1] < 0) OR (i = MAX_PAIRS));
+			for i := index to (MAX_PAIRS - 1) do {Just make the value of every element from the index equal to the value of the next element}
+				begin
+					p.pred_list[i,1] := p.pred_list[i+1, 1];
+					p.pred_list[i,2] := p.pred_list[i+1, 2];
+				end;
 			p.elements := p.elements - 1;
 		end;
 
@@ -122,8 +121,8 @@ implementation
 			i := 1;
 			while (i <= p.elements) do
 				begin
-					if(not board[p.pred_list[i, 1]-1, p.pred_list[i, 2]-1].in_memory) then
-						popVal(i, p)
+					if(board[p.pred_list[i, 1]-1, p.pred_list[i, 2]-1].status = clear) then
+						popVal(i, p) {Pop any value for which the board cell is already being used}
 					else
 					 i := i + 1;
 				end;
@@ -149,11 +148,11 @@ implementation
 					j := j + 1;
 				end;
 				i := i + 1;
-			end;
-		if (not found) then x1 := -13;
+			end; {A basic linear search, as the prediction list will not be sorted}
+		if (not found) then x1 := -13; {enter the placeholder value if no matching symbol is found}
 	end;
 
-	function RandomRange(Low, High: LongInt) : LongInt;
+	function RandomRange(Low, High: LongInt) : LongInt; {Self-explanatory}
 		begin
 		  if High < Low then
 		    RandomRange := High + Random(High - Low)
@@ -162,15 +161,17 @@ implementation
 		end;
 
 
-	function evaluate_bot_turn(n : Integer; game_board : matrix;  available : statusTracker; var p : Player) : botC;
+	function evaluate_bot_turn(prediction_status : Boolean; n : Integer; game_board : matrix;  available : statusTracker; var p : Player) : coordinate_array;
 	var
 		i, j, toAdd : Integer;
-		ret : botC;
+		ret : coordinate_array;
 	begin
 		ret[1] := -13;
-		if p.elements > 1 then
+		clean_memory(game_board, p); 
+		if p.elements > 1 then { Try to find matching if there are two or more values in memory}
 			find_matching(game_board, p, ret[1], ret[2], ret[3], ret[4]);
-		if (ret[1] < 0) then
+		
+		if (ret[1] < 0) then {If no matching symbol is found, give back a random prediction}
 			begin
 				i := RandomRange(low(available), length(available));
 				ret[1] := available[i].x;
@@ -181,18 +182,38 @@ implementation
 				ret[3] := available[j].x;
 				ret[4] := available[j].y;
 			end;
-		if (p.level = 5) then toAdd := random(4) else toAdd := p.level;
+		if ( (not prediction_status) AND (length(available) > 20) ) then  {Don't add anything new if the previous prediction was correct or if there are less than}
+			begin															{21 cells available. This value was found experimentally efficient}
+				if (p.level = 5) then toAdd := random(4) else toAdd := p.level;
 			
-		for i := 1 to toAdd do
-			begin
-				repeat
-					j := RandomRange(low(available), high(available));
-				until (game_board[available[j].x-1, available[j].y-1].in_memory = False);
-				p.elements := p.elements + 1;
-				p.pred_list[p.elements, 1] := available[j].x;
-				p.pred_list[p.elements, 2] := available[j].y;
-				game_board[available[j].x-1, available[j].y-1].in_memory := True;
-				popCell(j, available); 
+				if (toAdd < length(available)) then {If there are less available ones than there are to add than just add all of them.}
+					begin                            {This state won't be reached logically but was places as security, and fun, and because I am lazy.}
+						i := 1;
+						while ( (i <= toAdd) AND (length(available) > 20) ) do
+							begin
+								repeat
+									j := RandomRange(low(available), high(available));
+								until (game_board[available[j].x-1, available[j].y-1].in_memory = False);
+								p.elements := p.elements + 1;
+								p.pred_list[p.elements, 1] := available[j].x;
+								p.pred_list[p.elements, 2] := available[j].y;
+								game_board[available[j].x-1, available[j].y-1].in_memory := True;
+								popCell(j, available);
+								i := i + 1;
+							end;
+					end
+				else
+					begin
+						j := length(available);
+						for i := low(available) to high(available) do
+							begin
+								p.elements := p.elements + 1;
+								p.pred_list[p.elements, 1] := available[i].x;
+								p.pred_list[p.elements, 2] := available[i].y;
+								game_board[available[i].x-1, available[i].y-1].in_memory := True;
+							end;
+						for i := 1 to j do popCell(low(available), available);
+					end;	
 			end;
 		evaluate_bot_turn := ret;
 	end;
